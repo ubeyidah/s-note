a53a85cebb6704ff8a89f8969679057283dd6734
ubeyid oumer <ubeyidah@gmail.com>
2024-06-11 21:20:02 +0300

    use better background colors

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,14 @@ const App = () => {
 
   return (
     <>
-      <main className="bg-gradient-to-r from-cyan-400 to-cyan-700 min-h-screen font-serif relative pb-36">
-        <h1 className="text-center font-karlaB text-4xl text-white pt-10 pb-2">
+      <main className=" relative min-h-screen font-serif pb-36">
+        <div class="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div>
+        </div>
+        <h1 className="text-center font-karlaB text-4xl text-purple-500 pt-10 pb-2">
           S NOTE APP
         </h1>
-        <p className="text-center text-lg font-karla text-slate-200">
+        <p className="text-center text-lg font-karla text-slate-600">
           duble click on note to delete.
         </p>
         <section className="p-6 grid grid-cols-1 justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
